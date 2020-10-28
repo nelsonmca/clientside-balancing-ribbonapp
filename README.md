69e15f6a537304fad5539c75a92b2440bb3272c5
Nelson Nadar <nelsonnadar@nelsons-mbp.home>
2020-10-28 12:30:41 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# clientside-balancing-ribbonapp
---
+++ src/test/java/io/nelson/nadar/ribbontimeapp/RibbonTimeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nelson.nadar.ribbontimeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RibbonTimeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
